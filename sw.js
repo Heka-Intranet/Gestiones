b7bde33c329b9927672bec6197a93e27e7439aa2
Heka <logistica@hekaentrega.co>
2026-09-15 20:53:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'heka-panelS-v20'; // ← CAMBIA esto en cada despliegue
+const CACHE_NAME = 'heka-panelS-v21'; // ← CAMBIA esto en cada despliegue
 const ASSETS_TO_CACHE = [
   './',
   './manifest.json'
